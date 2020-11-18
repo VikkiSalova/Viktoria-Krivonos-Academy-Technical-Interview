039050df5943b586e125fcad56320afc35b1bcba
Viktoria <viktoria.krivonos@drupalsquad.com>
2020-11-18 18:53:32 +0300

    Final commit

--- algorithms-task/solution.php
+++ algorithms-task/solution.php
@@ -1,5 +1,14 @@
-function solution(int $n){
-    $a = ($n-1)/2;
+function fileProcessor() {
+    $cases = file('input.txt');
+    $result = fopen("output.txt", "w");
+    foreach($cases as $case){
+        fwrite($result, solution($case));
+    }
+    fclose($result);
+}
+
+function solution(int $size){
+    $steps = ($size-1)/2;
     $result = 0;
     
     for ($a; $a>0; $a--) {
